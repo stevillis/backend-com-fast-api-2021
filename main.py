5bca914bd29f29642ad2556108b11788c0ed5201
Stévillis Sousa <stevillis@hotmail.com>
2022-02-12 17:22:50 -0400

    Create path parameter

--- main.py
+++ main.py
@@ -4,5 +4,17 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def home():
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    message = f'Hello {name}'
+    return {"message": message}
+
+
+@app.get('/square/{number}')
+async def square(number: int):
+    result = f'The equare of {number} is {number ** 2}'
+    return {"result": result}
